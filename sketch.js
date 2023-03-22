fe7607d7c0125e3e2517fd66074946285ca13bc7
Beanhunter69 <113664194+Beanhunter69@users.noreply.github.com>
2023-03-22 10:29:10 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,35 +1,32 @@
-let x = 100;
+let x_rect = 100;
 let ball1;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ball1 = new Ball(100, 100, 5);
+}
 
-  function setup() {
-    createCanvas(windowWidth, windowHeight);
-    ball1 = new Ball(100, 100, 5);
+function draw() {
+  background(150);
+  rect(10, x_rect, 20, 120);
+  if (keyCode == 87) {
+    x_rect--;
+  }
+  if (keyCode == 83) {
+    x_rect++;
+  }
+  draw();
+}
+
+class Ball {
+  constructor(x, y, v) {
+    this.x = x;
+    this.y = y;
+    this.speed = v;
+    this.radius = 50;
   }
 
-  function draw() {
-    background(150);
-    rect(10, x, 20, 120);
-    if (keyCode == 87) {
-      x--;
-    }
-    if (keyCode == 83) {
-      x++;
-    }
-    draw();
-  }
-
-  class Ball {
-    constructor(x, y, v) {
-      this.x = x;
-      this.y = y;
-      this.speed = v;
-      this.radius = 50;
-    }
-
-    draw() {
-      ellipse(this.x, this.y, this.radius, this.radius);
-    }
+  draw() {
+    ellipse(this.x, this.y, this.radius, this.radius);
   }
 }
