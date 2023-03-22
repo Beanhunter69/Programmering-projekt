4ac7f4e83c3f4482ac7e77a6ff2f6a4bf5b97b30
Beanhunter69 <113664194+Beanhunter69@users.noreply.github.com>
2023-03-22 10:24:22 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,15 @@
+let x = 100;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    
 }
 
 function draw() {
   background(150);
+  rect(10, x, 20, 120);
+  if (keyCode == 87) {
+    x--;
+  }
+  if (keyCode == 83) {
+    x++;
+  }
 }
