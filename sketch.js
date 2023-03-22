0d4215f71464170f21d57eca57a0fd3bfcf7356f
timscn123 <timhlaug90@gmail.com>
2023-03-22 10:23:47 +0100

    created basic class Ball

--- sketch.js
+++ sketch.js
@@ -1,8 +1,24 @@
+let ball1;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    
+    ball1 = new Ball(100, 100, 5);
 }
 
 function draw() {
   background(150);
+  draw()
+}
+
+class Ball {
+  constructor(x, y, v) {
+    this.x = x;
+    this.y = y;
+    this.speed = v;
+    this.radius = 50;
+  }
+  
+  draw() {
+    ellipse(this.x, this.y, this.radius, this.radius);
+  }
 }
