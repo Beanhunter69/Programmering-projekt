054e829145513043ac2034af2b3d485d8336f0b0
Beanhunter69 <113664194+Beanhunter69@users.noreply.github.com>
2023-03-29 14:36:31 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,19 @@
 let x_rect = 100;
 let ball1;
+let state;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ball1 = new Ball(100, 100, 5);
+  state = new Position();
 }
 
 function draw() {
   background(150);
   rect(10, x_rect, 20, 120);
-  if (keyCode == 87) {
-    x_rect--;
-  }
-  if (keyCode == 83) {
-    x_rect++;
-  }
-  draw();
+  ball1.show();
+  view(state);
+  //print(state);
 }
 
 class Ball {
@@ -23,10 +21,66 @@ class Ball {
     this.x = x;
     this.y = y;
     this.speed = v;
-    this.radius = 50;
+    this.r = 25;
   }
+  show() {
+    ellipse(this.x, this.y, this.r, this.r);
+  }
+}
+class Position {
+  constructor() {
+    this.currentState = "idle";
+  }
+  transition(event) {
+    switch (this.currentState) {
+      case "idle":
+        if (event == "up") {
+          this.currentState = "op";
+        } else if (event == "down") {
+          this.currentState = "ned";
+        }
+        break;
+      case "up":
+        if (event === "stop") {
+          this.currentState = "idle";
+        }
+        break;
+      case "down":
+        if (event === "stop") {
+          this.currentState = "idle";
+        }
+        break;
+    }
+  }
+}
 
-  draw() {
-    ellipse(this.x, this.y, this.radius, this.radius);
+function view(model) {
+  switch (model.currentState) {
+    case "op":
+      x_rect--;
+      break;
+    case "ned":
+      x_rect++;
+      break;
+  }
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 87:
+      state.transition("up");
+      break;
+    case 83:
+      state.transition("down");
+      break;
+  }
+}
+function keyReleased() {
+  switch (keyCode) {
+    case 87:
+      state.transition("stop");
+    case 83:
+      state.transition("stop");
+      break;
   }
 }
